e9c77a9a6d0e873044d371a1e76f6574deea92eb
RezhaArifin <rezhaarifin782@gmail.com>
2023-10-10 13:48:51 +0700

    zha dan man

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,7 +11,7 @@
 <body>
 	<header>
 		<nav>
-			<div class="logo">Wealth</div>
+			<div class="logo">Zha</div>
 			<ul class="nav-links">
 				<li><a href="#">Home</a></li>
 				<li><a href="#">Log Out</a></li>
@@ -23,14 +23,14 @@
 		<h1>About Us</h1>
 		<p style="font-weight: bold">GeeksforGeeks is a leading platform...</p>
 		<div class="about-info">
-			
+
 			<div>
-	<p>GeeksforGeeks is a leading platform that provides computer science resources
-				and coding challenges for programmers and technology enthusiasts,
-				along with interview and exam preparations for upcoming aspirants.
-				With a strong emphasis on enhancing coding skills and knowledge,
-				it has become a trusted destination for over 12 million plus registered
-				users worldwide.</p>
+				<p>GeeksforGeeks is a leading platform that provides computer science resources
+					and coding challenges for programmers and technology enthusiasts,
+					along with interview and exam preparations for upcoming aspirants.
+					With a strong emphasis on enhancing coding skills and knowledge,
+					it has become a trusted destination for over 12 million plus registered
+					users worldwide.</p>
 				<button>Read More...</button>
 			</div>
 		</div>
@@ -43,8 +43,7 @@
 			<!-- Card 1 -->
 			<div class="card">
 				<div class="card-img">
-					<img src=
-"https://media.geeksforgeeks.org/wp-content/uploads/20230824122630/
+					<img src="https://media.geeksforgeeks.org/wp-content/uploads/20230824122630/
 business-office-business-woman-professional.jpg" alt="User 1">
 				</div>
 				<div class="card-info">
@@ -58,8 +57,7 @@ business-office-business-woman-professional.jpg" alt="User 1">
 			<!-- Card 2 -->
 			<div class="card">
 				<div class="card-img">
-					<img src=
-"https://media.geeksforgeeks.org/wp-content/uploads/20230822183347/
+					<img src="https://media.geeksforgeeks.org/wp-content/uploads/20230822183347/
 man-portrait-businessman-male.jpg" alt="User 2">
 				</div>
 				<div class="card-info">
@@ -72,8 +70,7 @@ man-portrait-businessman-male.jpg" alt="User 2">
 			<!-- Card 3 -->
 			<div class="card">
 				<div class="card-img">
-					<img src=
-"https://media.geeksforgeeks.org/wp-content/uploads/20230824122630/
+					<img src="https://media.geeksforgeeks.org/wp-content/uploads/20230824122630/
 business-office-business-woman-professional.jpg" alt="User 3">
 				</div>
 				<div class="card-info">
@@ -93,250 +90,251 @@ business-office-business-woman-professional.jpg" alt="User 3">
 
 </html>
 <style>
-    * {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
-/* Global Styles */
-body {
-	font-family: Arial, sans-serif;
-}
-/* Header */
-header {
-	background-color: white;
-	color: rgb(0, 0, 0);
-	padding: 10px 0;
-	position: fixed;
-	width: 100%;
-	z-index: 100;
-	box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
 
-nav {
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	margin: 5px auto;
-	padding: 0 20px;
-}
+	/* Global Styles */
+	body {
+		font-family: Arial, sans-serif;
+	}
 
-.logo {
-	font-size: 1.5rem;
-	font-weight: bold;
-	color: #005689;
-}
+	/* Header */
+	header {
+		background-color: white;
+		color: rgb(0, 0, 0);
+		padding: 10px 0;
+		position: fixed;
+		width: 100%;
+		z-index: 100;
+		box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+	}
 
-.nav-links {
-	list-style: none;
-	display: flex;
-}
-
-.nav-links li {
-	margin-right: 20px;
-}
-
-.nav-links a {
-	color: rgb(0, 0, 0);
-	padding: 10px;
-	font-weight: bold;
-	text-decoration: none;
-}
-
-.nav-links a:hover {
-	background-color: #555;
-	border-radius: 4px;
-	color: white;
-}
-
-/* About Section */
-.about {
-    background: rgb(19,192,232);
-    background: linear-gradient(90deg, rgba(19,192,232,1) 0%, rgba(1,134,255,1) 100%, rgba(0,212,255,1) 100%);
-	padding: 100px 0 20px 0;
-	text-align: center;
-}
-
-.about h1 {
-	font-size: 2.5rem;
-	margin-bottom: 20px;
-}
-
-.about p {
-	font-size: 1rem;
-	color: #222;
-	max-width: 800px;
-	margin: 0 auto;
-}
-
-.about-info {
-	margin: 2rem 2rem;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	text-align: left;
-}
-
-.about-img {
-	width: 20rem;
-	height: 20rem;
-
-}
-
-.about-img img {
-	width: 100%;
-	height: 100%;
-	border-radius: 5px;
-	object-fit: contain;
-}
-
-.about-info p {
-	font-size: 1.3rem;
-	margin: 0 2rem;
-	text-align: justify;
-}
-
-button {
-	border: none;
-	outline: 0;
-	padding: 10px;
-	margin: 2rem;
-	font-size: 1rem;
-	color: #fff;
-	background-color: #007cb9;
-	text-align: center;
-	cursor: pointer;
-	width: 15rem;
-	border-radius: 4px;
-}
-
-button:hover {
-	background-color: #555;
-}
-
-/* Team Section */
-.team {
-	padding: 30px 0;
-	text-align: center;
-}
-
-.team h1 {
-	font-size: 2.5rem;
-	margin-bottom: 20px;
-}
-
-.team-cards {
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: center;
-	gap: 15px;
-	margin-top: 20px;
-}
-
-.card {
-	background-color: white;
-	border-radius: 6px;
-	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
-	overflow: hidden;
-	transition: transform 0.2s, box-shadow 0.2s;
-	width: 18rem;
-	height: 25rem;
-	margin-top: 10px;
-}
-
-.card:hover {
-	transform: translateY(-5px);
-	box-shadow: 0 8px 12px rgba(0, 0, 0, 0.5);
-}
-
-.card-img {
-	width: 18rem;
-	height: 12rem;
-}
-
-.card-img img {
-	width: 100%;
-	height: 100%;
-	object-fit: fill;
-}
-
-.card-info button {
-	margin: 2rem 1rem;
-}
-
-.card-name {
-	font-size: 2rem;
-	margin: 10px 0;
-}
-
-.card-role {
-	font-size: 1rem;
-	color: #888;
-	margin: 5px 0;
-}
-
-.card-email {
-	font-size: 1rem;
-	color: #555;
-}
-
-/* Footer */
-footer {
-	background-color: #222;
-	color: white;
-	text-align: center;
-	padding: 20px 0;
-}
-
-@media (max-width: 768px) {
 	nav {
-		display: block;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: 5px auto;
+		padding: 0 20px;
 	}
 
 	.logo {
-		text-align: center;
+		font-size: 1.5rem;
+		font-weight: bold;
+		color: #005689;
 	}
 
 	.nav-links {
-		margin-top: 1rem;
-		justify-content: space-between;
+		list-style: none;
+		display: flex;
 	}
 
 	.nav-links li {
-		margin-right: 0;
+		margin-right: 20px;
 	}
 
-	.about h1 {
-		font-size: 2rem;
+	.nav-links a {
+		color: rgb(0, 0, 0);
+		padding: 10px;
+		font-weight: bold;
+		text-decoration: none;
 	}
 
-	.about p {
-		font-size: 0.9rem;
+	.nav-links a:hover {
+		background-color: #555;
+		border-radius: 4px;
+		color: white;
 	}
 
-	.about-info {
-		flex-direction: column;
+	/* About Section */
+	.about {
+		background: rgb(19, 192, 232);
+		background: linear-gradient(90deg, rgba(19, 192, 232, 1) 0%, rgba(1, 134, 255, 1) 100%, rgba(0, 212, 255, 1) 100%);
+		padding: 100px 0 20px 0;
 		text-align: center;
 	}
 
+	.about h1 {
+		font-size: 2.5rem;
+		margin-bottom: 20px;
+	}
+
+	.about p {
+		font-size: 1rem;
+		color: #222;
+		max-width: 800px;
+		margin: 0 auto;
+	}
+
+	.about-info {
+		margin: 2rem 2rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-align: left;
+	}
+
 	.about-img {
-		width: 60%;
-		height: 60%;
-		margin-bottom: 1rem;
+		width: 20rem;
+		height: 20rem;
+
+	}
+
+	.about-img img {
+		width: 100%;
+		height: 100%;
+		border-radius: 5px;
+		object-fit: contain;
 	}
 
 	.about-info p {
-		margin: 1rem 2rem;
+		font-size: 1.3rem;
+		margin: 0 2rem;
+		text-align: justify;
 	}
 
-	.about-info button {
-		margin: 1rem 2rem;
-		width: 10rem;
+	button {
+		border: none;
+		outline: 0;
+		padding: 10px;
+		margin: 2rem;
+		font-size: 1rem;
+		color: #fff;
+		background-color: #007cb9;
+		text-align: center;
+		cursor: pointer;
+		width: 15rem;
+		border-radius: 4px;
 	}
 
+	button:hover {
+		background-color: #555;
+	}
+
+	/* Team Section */
 	.team {
-		margin: 0 1rem;
+		padding: 30px 0;
+		text-align: center;
 	}
-}
 
+	.team h1 {
+		font-size: 2.5rem;
+		margin-bottom: 20px;
+	}
+
+	.team-cards {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		gap: 15px;
+		margin-top: 20px;
+	}
+
+	.card {
+		background-color: white;
+		border-radius: 6px;
+		box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
+		overflow: hidden;
+		transition: transform 0.2s, box-shadow 0.2s;
+		width: 18rem;
+		height: 25rem;
+		margin-top: 10px;
+	}
+
+	.card:hover {
+		transform: translateY(-5px);
+		box-shadow: 0 8px 12px rgba(0, 0, 0, 0.5);
+	}
+
+	.card-img {
+		width: 18rem;
+		height: 12rem;
+	}
+
+	.card-img img {
+		width: 100%;
+		height: 100%;
+		object-fit: fill;
+	}
+
+	.card-info button {
+		margin: 2rem 1rem;
+	}
+
+	.card-name {
+		font-size: 2rem;
+		margin: 10px 0;
+	}
+
+	.card-role {
+		font-size: 1rem;
+		color: #888;
+		margin: 5px 0;
+	}
+
+	.card-email {
+		font-size: 1rem;
+		color: #555;
+	}
+
+	/* Footer */
+	footer {
+		background-color: #222;
+		color: white;
+		text-align: center;
+		padding: 20px 0;
+	}
+
+	@media (max-width: 768px) {
+		nav {
+			display: block;
+		}
+
+		.logo {
+			text-align: center;
+		}
+
+		.nav-links {
+			margin-top: 1rem;
+			justify-content: space-between;
+		}
+
+		.nav-links li {
+			margin-right: 0;
+		}
+
+		.about h1 {
+			font-size: 2rem;
+		}
+
+		.about p {
+			font-size: 0.9rem;
+		}
+
+		.about-info {
+			flex-direction: column;
+			text-align: center;
+		}
+
+		.about-img {
+			width: 60%;
+			height: 60%;
+			margin-bottom: 1rem;
+		}
+
+		.about-info p {
+			margin: 1rem 2rem;
+		}
+
+		.about-info button {
+			margin: 1rem 2rem;
+			width: 10rem;
+		}
+
+		.team {
+			margin: 0 1rem;
+		}
+	}
 </style>
